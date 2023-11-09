fb0a2ecb9ef63f8379b28eb51a6b1123b57ab5ed
unknown <csarevalo1@gmail.com>
2023-11-08 22:42:14 -0800

    removed unnecessary vars

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/src/calculator_app.dart';
+import 'package:calculator_app/src/calculator_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
